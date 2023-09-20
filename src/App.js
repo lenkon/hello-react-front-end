622ddabb66ba90c0c3ab5193b37d98ad3f6063a3
inoulen inoulen <inoulenkon@gmail.com>
2023-09-20 10:43:34 +0530

    Add routes and navbar to App

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Greeting from './components/Greeting';
 import Home from './components/Home';
-import NaVBar from './components/NavBar';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome</h1>
+        <NavBar />
+        <Routes>
+          <Route exact path="/*" element={<Home />} />
+          <Route exact path="/greet" element={<Greeting />} />
+        </Routes>
       </header>
     </div>
   );
