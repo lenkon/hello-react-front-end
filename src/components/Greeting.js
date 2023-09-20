889e5616aac5f61a3375074368f3fd3cabf2ecff
inoulen inoulen <inoulenkon@gmail.com>
2023-09-20 10:50:54 +0530

    Update greeting component to fetch from store

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchGreetingsThunk } from '../redux/slices/greetingSlice';
 
 const Greeting = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchGreetingsThunk());
+  }, []);
+
+  const greetings = useSelector((state) => state.greetings.greetings);
+  const { greet } = greetings;
+
   return (
     <div>
-        <h1>Greetings!</h1>
+      <h1>{greet}</h1>
     </div>
-  )
+  );
 };
 
 export default Greeting;
